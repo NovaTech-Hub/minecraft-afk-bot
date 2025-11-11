0c447b618914400704afcf6f5a97951e56d45dde
NovaTech-Hub <jaydenmichaelclarke@gmail.com>
2025-11-11 22:06:11 +0000

    Add web server and Minecraft bot functionality
    This script creates a web server that responds with 'I'm alive!' and a Minecraft bot that reconnects after being kicked or disconnected.

---
+++ index.js
@@ -0,0 +1,48 @@
+// Part 1: The Web Server (to keep it awake)
+const http = require('http' );
+http.createServer(function (req, res ) {
+  res.write("I'm alive!"); // The message UptimeRobot will see
+  res.end();
+}).listen(8080);
+
+// Part 2: The Minecraft Bot
+const mineflayer = require('mineflayer');
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'MentallyStable2SMP.play.hosting.play.hosting', // <-- Your server IP
+    username: 'AFK_Bot_247', // <-- Your bot's name
+    // If your server is in "online-mode", this will be more complex.
+    // For now, we assume it's "offline-mode".
+  });
+
+  bot.on('login', () => {
+    console.log('Bot has logged in.');
+    // This makes the bot jump every 30 seconds to avoid being kicked for AFK
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      bot.setControlState('jump', false);
+    }, 30000);
+  });
+
+  bot.on('kicked', (reason) => {
+    console.log('Bot was kicked for:', reason);
+    // Try to reconnect after 5 minutes
+    setTimeout(createBot, 5 * 60 * 1000);
+  });
+
+  bot.on('error', (err) => {
+    console.log('An error occurred:', err);
+    // Try to reconnect after 5 minutes
+    setTimeout(createBot, 5 * 60 * 1000);
+  });
+
+  bot.on('end', () => {
+    console.log('Bot has disconnected.');
+    // Try to reconnect after 5 minutes
+    setTimeout(createBot, 5 * 60 * 1000);
+  });
+}
+
+// Start the bot for the first time
+createBot();
